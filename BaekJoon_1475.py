1dac036a0590975525a9993a9d56ef3d7ec0afc3
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-09-07 10:20:05 +0900

    Create BaekJoon_1475.py

---
+++ BaekJoon_1475.py
@@ -0,0 +1,8 @@
+# BaekJoon 1475
+# https://www.acmicpc.net/problem/1475
+
+num = [0] * 9
+room = input().replace('9', '6')
+for n in room: num[int(n)] += 1
+num[6] = num[6]//2 + num[6] % 2
+print(max(num))
