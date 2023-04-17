5ddbb63e8bb9b345fe166e4d027eb7de486b1a42
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-04-17 20:28:36 +0900

    [Silver II] Title: 스택 수열, Time: 156 ms, Memory: 41336 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,24 @@
+from sys import stdin
+n = int(input())
+targets = [int(stdin.readline().rstrip()) for _ in range(n)]
+stk, R, result = [], [], []
+i = 1; idx = 0
+while True:
+    if i == n + 1:
+        if not stk: break
+        elif targets[idx] != stk[-1]: break
+
+    if stk == []:
+        stk.append(i)
+        result.append('+')
+        i += 1
+    if targets[idx] == stk[-1]:
+        R.append(stk.pop())
+        result.append('-')
+        idx += 1
+    else:
+        stk.append(i)
+        result.append('+')
+        i += 1
+if R == targets: print('\n'.join(result))
+else: print('NO')
