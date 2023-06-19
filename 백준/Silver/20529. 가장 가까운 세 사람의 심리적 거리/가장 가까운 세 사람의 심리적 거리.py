f19e627de60b8e44c4208db91565c4660c8fe4df
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-06-19 17:13:49 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 656 ms, Memory: 39236 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from itertools import combinations
+
+def psychological_distance(mbti1, mbti2):
+    return sum([n != m for n, m in zip(mbti1, mbti2)])
+
+for t in range(int(input())):
+    N = int(input())
+    MBTI = list(input().split())
+    if max(Counter(MBTI).values()) >= 3:
+        print(0)
+    else:
+        distances = []
+        for i in range(len(MBTI) - 2):
+            for j in range(i + 1, len(MBTI) - 1):
+                for k in range(j + 1, len(MBTI)):
+                    distances.append(psychological_distance(MBTI[i], MBTI[j]) + psychological_distance(MBTI[j], MBTI[k]) + psychological_distance(MBTI[k], MBTI[i]))
+        print(min(distances))
