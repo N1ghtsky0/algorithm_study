efd1272491fb7e38965d26250591d0548942a54e
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2026-01-11 21:11:19 +0900

    [level 2] Title: 미로 탈출, Time: 6.37 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -4,69 +4,48 @@ class Solution {
     final int[] dy = {0, -1, 0, 1};
     
     public int solution(String[] maps) {
-        int maxX = maps[0].length();
-        int maxY = maps.length;
-        boolean[][] visited = new boolean[maxY][maxX];
+        int startX = 0;
+        int startY = 0;
         
-        Queue<int[]> steps = new LinkedList<>();
-        int answer = -1;
-        //1. 시작지점 찾기
         for (int r = 0; r < maps.length; r++) {
             for (int c = 0; c < maps[r].length(); c++) {
                 if ('S' == maps[r].charAt(c)) {
-                    visited[r][c] = true;
-                    steps.offer(new int[]{r, c, 0});
+                    startX = c;
+                    startY = r;
                     break;
                 }
             }
         }
         
-        //2. 레버까지 최단거리 찾기
-        boolean findLever = false;
-        while (!findLever && !steps.isEmpty()) {
+        int[] distanceToLever = findShortestDistance(maps, startX, startY, 'L');
+        if (distanceToLever == null) { return -1; }
+        int[] distanceToExit = findShortestDistance(maps, distanceToLever[1], distanceToLever[0], 'E');
+        if (distanceToExit == null) { return -1; }
+        return distanceToLever[2] + distanceToExit[2];
+    }
+    
+    private int[] findShortestDistance(String[] maps, int startX, int startY , char target) {
+        int maxX = maps[0].length();
+        int maxY = maps.length;
+        boolean[][] visited = new boolean[maxY][maxX];
+        
+        Queue<int[]> steps = new LinkedList<>();
+        steps.add(new int[]{startY, startX, 0});
+        while (!steps.isEmpty()) {
             int[] step = steps.poll();
             int nextStep = ++step[2];
             for (int d = 0; d < 4; d++) {
                 int y = step[0] + dy[d];
                 int x = step[1] + dx[d];
                 if (0 <= y && y < maxY && 0 <= x && x < maxX && 'X' != maps[y].charAt(x) && !visited[y][x]) {
-                    steps.offer(new int[]{y, x, nextStep});
+                    steps.add(new int[]{y, x, nextStep});
                     visited[y][x] = true;
-                    if ('L' == maps[y].charAt(x)) {
-                        findLever = true;
-                        answer = nextStep;
-                        steps.clear();
-                        steps.offer(new int[]{y, x, 0});
-                        visited = new boolean[maxY][maxX];
-                        break;
+                    if (target == maps[y].charAt(x)) {
+                        return new int[]{y, x, nextStep};
                     }
                 }
             }
         }
-        
-        if (!findLever) { return -1; }
-        
-        //3. 레버부터 출구까지 최단거리 찾기
-        boolean findExit = false;
-        while (!findExit && !steps.isEmpty()) {
-            int[] step = steps.poll();
-            int nextStep = ++step[2];
-            for (int d = 0; d < 4; d++) {
-                int y = step[0] + dy[d];
-                int x = step[1] + dx[d];
-                if (0 <= y && y < maxY && 0 <= x && x < maxX && 'X' != maps[y].charAt(x) && !visited[y][x]) {
-                    steps.offer(new int[]{y, x, nextStep});
-                    visited[y][x] = true;
-                    if ('E' == maps[y].charAt(x)) {
-                        findExit = true;
-                        answer += nextStep;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        if (!findExit) { return -1; }
-        return answer;
+        return null;
     }
 }
